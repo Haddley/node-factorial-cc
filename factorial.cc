f08a66784a0ffd50409c57d1c2ec257fa4012ba6
Neil Haddley <neil@haddley.com>
2021-02-25 23:47:39 +1300

    Update factorial.cc

--- factorial.cc
+++ factorial.cc
@@ -1,46 +1,43 @@
 // factorial.cc
 #include <node.h>
 
-namespace haddley
-{
+namespace haddley {
 
-    using v8::FunctionCallbackInfo;
-    using v8::Isolate;
-    using v8::Local;
-    using v8::Object;
-    using v8::String;
-    using v8::Value;
-    using v8::Number;
-    using v8::Uint32;
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::Number;
+using v8::Object;
+using v8::String;
+using v8::Uint32;
+using v8::Value;
 
-    double fac(int n)
-    {
-        if (n >= 1)
-            return n * fac(n - 1);
-        else
-            return 1;
-    }
+double fac(int n) {
+  if (n >= 1)
+    return n * fac(n - 1);
+  else
+    return 1;
+}
 
-    void Method(const FunctionCallbackInfo<Value> &args)
-    {
-        Isolate * isolate = args.GetIsolate();
+void Method(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
 
-        int n = (int)(unsigned)args[0]->NumberValue();
+  Local<v8::Context> context = isolate->GetCurrentContext();
 
-        double value = fac(n);
+  double d =  args[0]->NumberValue(context).ToChecked();
+  int n = (int)(unsigned)d;
 
-        Local<Number> retval = Number::New(isolate, value);
-        
-        args.GetReturnValue().Set(retval);
+  double value = fac(n);
 
+  Local<Number> retval = Number::New(isolate, value);
 
-    }
+  args.GetReturnValue().Set(retval);
+}
 
-    void init(Local<Object> exports)
-    {
-        NODE_SET_METHOD(exports, "factorial", Method);
-    }
+void init(Local<Object> exports) {
+  NODE_SET_METHOD(exports, "factorial", Method);
+}
 
-    NODE_MODULE(factorial, init)
+NODE_MODULE(factorial, init)
 
-} // namespace haddley
+}  // namespace haddley
